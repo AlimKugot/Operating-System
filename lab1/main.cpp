5e6198f3de1fa4a6cd7905e2a5c4fc7a8b95063d
Alim <alim.filipov@gmail.com>
2022-02-18 16:47:28 +0300

    Added bash scrypts

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 bool isEnd1 = false;
 bool isEnd2 = false;
 
-void* proc1(void * pointer) {
+void* proc1(void* pointer) {
 	cout << "Start proc1" << endl;
 	int count = 0;
 	while (!isEnd1) {
 		count++;
 	}
-	cout << "End proc1 with count : " << count << endl;
+	cout << "End proc1 with count: " << count << endl;
 	pthread_exit(NULL);
 }
 
@@ -22,21 +22,23 @@ void* proc2(void* pointer) {
 	while (!isEnd2) {
 		count++;
 	}	
-	cout << "End proc2 with count : " << count << endl;
+	cout << "End proc2 with count: " << count << endl;
 	pthread_exit(NULL);
 }
 
 int main() {
+	cout << "Starting c++ program" << endl;
+
 	const int NUM_THREADS = 2;
 	pthread_t threads[NUM_THREADS];
 
-	cout << "created thread 1\n";
+	cout << "created thread 1" << endl;
 	if (pthread_create(&threads[0], NULL, proc1, NULL) == -1) {
 	       	cout << "Cannot create thread 1" << endl;
 		return -1;
 	}
 
-	cout << "created thread 2\n";
+	cout << "created thread 2" << endl;
 	if (pthread_create(&threads[1], NULL, proc2, NULL) == -1) {
 		cout << "Cannot create thread 2" << endl;
 		return -1;
