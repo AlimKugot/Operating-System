39539566de9f82ede1ea4c10a9f8a50abb638417
Alim <alim.filipov@gmail.com>
2022-03-26 23:09:20 +0300

    added sem_trywait & sem_timedlock

--- lab2/sem_timedwait.cpp
+++ lab2/sem_timedwait.cpp
@@ -2,54 +2,68 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <time.h>
 
 using namespace std;
 
 sem_t semaphore;
+
 
 int PROC_1_EXIT_SUCCESS = 1;
 int PROC_2_EXIT_SUCCESS = 2;
 
 
 void* proc1(void* isEnd) {
-	int count = 0;
-
+	timespec tp;
 	while (!(*((bool*) isEnd))) {
-		cout << "Starting locking semaphore 1" << endl;
-		sem_wait(&semaphore);
+		clock_gettime(CLOCK_REALTIME, &tp);
+		tp.tv_sec += 2;
+		while (sem_timedwait(&semaphore, &tp) != 0) {
+			cout << "trying to unlock semaphore 1 failed" << endl;
+			tp.tv_sec += 2;
+		}
+		cout << "locked semaphore 1" << endl;
 		for (int i = 0; i < 5; i++) {
-			cout << 1;
+			cout << 1 << endl;
 			sleep(1);
 		}
-		cout << " ";
+		cout << "unlocked semaphore 1" << endl;
 		sem_post(&semaphore);
+		sleep(1);
 	}
-
 	pthread_exit(&PROC_1_EXIT_SUCCESS);
 }
 
 
 void* proc2(void* isEnd) {
-
+	timespec tp;
 	while (!(*((bool*) isEnd))) {
-		cout << "Starting locking semaphore 2" << endl;
-		sem_wait(&semaphore);
+		clock_gettime(CLOCK_REALTIME, &tp);
+		tp.tv_sec += 2;
+		while (sem_timedwait(&semaphore, &tp) != 0) {
+			cout << "trying to unlock semaphore 2 failed" << endl;
+			tp.tv_sec += 2;
+		}
+		cout << "locked semaphore 2" << endl;
 		for (int i = 0; i < 5; i++) {
-			cout << 2;
+			cout << 2 << endl;
 			sleep(1);
 		}
-		cout << " ";
+		cout << "unlocked semaphore 2" << endl;
 		sem_post(&semaphore);
+		sleep(1);
 	}
-
 	pthread_exit(&PROC_2_EXIT_SUCCESS);
 }
+
 
 int main() {
 	cout << "Starting c++ program" << endl;
 	
 	// from https://docs.oracle.com/cd/E19455-01/806-5257/sync-42602/index.html
-	// When pshared is nonzero, the semaphore can be shared by other processes.
+	// 'When pshared is nonzero, the semaphore can be shared by other processes.'
+	//	
+	// third arg = count of threads for this semaphore
 	sem_init(&semaphore, 0, 1);
 
 	bool* isEnd = new bool;
@@ -58,27 +72,31 @@ int main() {
 	const int NUM_THREADS = 2;
 	pthread_t threads[NUM_THREADS];
 
-	cout << "created thread 1" << endl;
-	if (pthread_create(&threads[0], NULL, proc1, isEnd) == -1) {
-	       	cout << "Cannot create thread 1" << endl;
+
+	if (pthread_create(&threads[0], NULL, proc1, isEnd) != -1) {
+		cout << "created thread 1" << endl;
+	} else {
+	       	cerr << "Cannot create thread 1" << endl;
+		return -1;
+	}
+	
+
+	if (pthread_create(&threads[1], NULL, proc2, isEnd) != -1) {
+		cout << "created thread 2" << endl;
+	} else {
+		cerr << "Cannot create thread 2" << endl;
 		return -1;
 	}
 
-	cout << "created thread 2" << endl;
-	if (pthread_create(&threads[1], NULL, proc2, isEnd) == -1) {
-		cout << "Cannot create thread 2" << endl;
-		return -1;
-	}
 
 	getchar();
-
 	*isEnd = true;
 
 	void* status1;
 	void* status2; 
 
-	int ret1 = pthread_join(threads[0], &status1);
 	int ret2 = pthread_join(threads[1], &status2);
+	int ret1 = pthread_join(threads[0], &status1);
 
 	sem_destroy(&semaphore);
 
