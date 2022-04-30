84ff988dc87a318f2ffc1cb1e3f133af5db186ba
Alim <alim.filipov@gmail.com>
2022-04-30 14:36:25 +0300

    first try

--- lab4/program1.cpp
+++ lab4/program1.cpp
@@ -3,19 +3,17 @@
 
 using namespace std;
 
-
-
-
 int main(int argc, char* argv[]) {
 	cout << string(20, '-') << endl;
 	cout << "Program 1. Start" << endl;
 	cout << "Program 1. Current ID: " << getpid() << endl; 
 	cout << "Program 1. Parent ID: " << getppid() << endl;
-	for (int i = 1; i <= 5; i++) {
-		cout << "Program 1." << "\t\t\t" << i << endl;
+
+	for (int i = 0; i < argc; i++) {
+		cout << "Progrma 1. Args: \t\t\t" << argv[i] << endl;
 		sleep(1);
 	}
 	cout << "Program 1. End" << endl;
 	cout << string(20, '-') << endl;
-	return 25;
+	return 0;
 }
--- lab4/program2.cpp
+++ lab4/program2.cpp
@@ -6,37 +6,19 @@
 using namespace std;
 
 
-
-string convert_argv_to_string(int argc, char* argv[]) {
-	string argv_line = "";
-	int i;
-	for (i = 0; i < argc - 1; i++) {
-		argv_line += string(argv[i]) + " ";
-	}
-	argv_line += argv[i];
-	return argv_line;
-}
-
-
 int main(int argc, char* argv[]) {
 	cout << "Program 2. Start" << endl;
 	cout << "Program 2. Curent ID: " << getpid() << endl;
 	cout << "Program 2. Parent ID: " << getppid() << endl;
 	int status;
 	pid_t pid;
-	string argv_line = convert_argv_to_string(argc, argv);
-	cout << argv_line << endl;
 
 	if ((pid = fork()) < 0) {
 		perror("fork error");
 	} else if (pid == 0) {
 		cout << "Program 2. Child ID: " << getpid() << endl;
-		//string terminal_comand = "g++ program1.cpp -o p1.out && . && ./p1.out " + argv_line;
-		//cout << terminal_comand << endl;
-		//execlp("sh", "sh", "-c", terminal_comand, NULL);
-		execlp("sh", "sh", "-c", "g++ program1.cpp -o p1.out && . && ./p1.out", NULL);
+		execve("./p1.out", argv, NULL);
 	} else do {
-		// do it repeatly
 		if ((pid = waitpid(pid, &status, WNOHANG)) == -1) {
 			perror("waiting error");
 		} else if (pid == 0) {
