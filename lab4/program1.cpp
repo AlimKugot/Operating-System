98e41659077ab1cb90e98b17cefc781ff97fd080
Alim <alim.filipov@gmail.com>
2022-05-14 23:48:42 +0300

    first try

--- lab4/program1.cpp
+++ lab4/program1.cpp
@@ -5,22 +5,16 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	cout << string(50, '.') << endl;
-	cout << "Program 1. Старт" << endl;
-	cout << "Program 1. Текущий ID: " << getpid() << endl; 
+	cout << "Program 1. Start" << endl;
+	cout << "Program 1. Current ID: " << getpid() << endl; 
 	cout << "Program 1. Parent ID: " << getppid() << endl;
 
-	const char* teacher = "Учитель";
-	const char* me = "Студентка";
-	const char* animal = "animal";
-	cout << "Program 1. " << teacher << " = "  << getenv(teacher) << endl;
-	cout << "Program 1. " << me << " = " <<  getenv(me) << endl;
-	cout << "Program 1. " << animal << ":" << endl << getenv(animal) << endl;
-
+	cout << "Program 1. Author = "  << getenv("author") << endl;
 	for (int i = 0; i < argc; i++) {
-		cout << "Progrma 1. Аргументы: " << argv[i] << endl;
+		cout << "Program 1. args: " << argv[i] << endl;
 		sleep(1);
 	}
-	cout << "Program 1. Конец" << endl;
+	cout << "Program 1. end" << endl;
 	cout << string(50, '.') << endl;
 	return 0;
 }
--- lab4/program2.cpp
+++ lab4/program2.cpp
@@ -2,63 +2,38 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define handle_error_en(en, msg) \
-               do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
-    cout << "Program 2. Старт" << endl;
-    cout << "Program 2. Текущий ID: " << getpid() << endl;
+    cout << "Program 2. Start" << endl;
+    cout << "Program 2. Current ID: " << getpid() << endl;
     cout << "Program 2. Parent ID: " << getppid() << endl;
-    int rv;
     pid_t pid;
 
     // error
     if ((pid = fork()) < 0) {
-        handle_error_en(0, "fork error");
-        // child
+	perror("fork error");
+	sleep(1);
+    // child
     } else if (pid == 0) {
         cout << "Program 2. Child ID: " << getpid() << endl;
-
-        string animal_key = "animal";
-        string animal_value =
-                    "┊┊┊┊┊╭╭╭╮╮╮┊┊┊┊ \n"
-                    "┊┊┊┊┊╰╰╲╱╯╯┊┊┊┊ \n"
-                    "┊┏╮╭┓╭━━━━━━╮┊┊ \n"
-                    "┊╰╮╭╯┃┈┈┈┈┈┈┃┊┊ \n"
-                    "┊┊┃╰━╯┈┈╰╯┈┈┃┊┊ \n"
-                    "┊┊┃┈┈┈┈┈┈┈╰━┫┊┊ \n"
-                    "╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲ \n";
-        string teacher_key = "Учитель";
-        string teacher_value = "Широков Владимир Владимирович";
-
-        string student_key = "Студентка";
-        string student_value = "Dina";
-
-        string animal =  animal_key + "=" + animal_value;
-        string teacher = teacher_key + "=" + teacher_value;
-        string student = student_key + "=" + student_value;
-
-        char* const animal_p = const_cast<char*>(animal.c_str());
-        char* const teacher_p = const_cast<char*>(teacher.c_str());
-        char* const student_p = const_cast<char*>(student.c_str());
-
-        char* const env[] = {animal_p, teacher_p, student_p, nullptr};
+        char* const env[] = {"author=Ekaterina", nullptr};
         execvpe("./p1.out", argv, env);
-        // parrent
+    // parrent
     } else do {
+	    int rv;
             if ((pid = waitpid(pid, &rv, WNOHANG)) == -1) {
-                handle_error_en(rv, "waiting error");
+		perror("waiting error");
             } else if (pid == 0) {
-                cout << "Program 2. <-- Проверка: дочерний процесс активен -->" << endl;
+                cout << "Program 2. <-- Check: child proccess is alive -->" << endl;
                 sleep(2);
             } else {
                 if (WIFEXITED(rv)) {
                     const int exit_status = WEXITSTATUS(rv);
                     cout << "Program 2. Child exited with status: " << exit_status << endl;
-                    cout << "Program 2. Конец" << endl;
+                    cout << "Program 2. End" << endl;
                 }
             }
         } while (pid == 0);
